b7c3453c4f333a7072dccc62cc4df202bfed458b
taachii <taachii21@outlook.com>
2025-03-19 13:08:00 +0100

    Limit display string size

--- script.js
+++ script.js
@@ -3,8 +3,7 @@ const digitButtons = document.querySelectorAll(".digit, .decimal");
 const clearButton = document.querySelector(".ac");
 const backButton = document.querySelector(".back")
 
-const maxDisplayStringLength = 9;
-let currentDisplayStringLength = 0;
+const maxDisplayStringLength = 10;
 let a = null;
 let b = null;
 let operator = null;
@@ -19,13 +18,14 @@ function addEventListeners() {
   digitButtons.forEach(button => {
     button.addEventListener("click", () => {
       const value = button.textContent.trim();
-      display.textContent += value;
+      if (display.textContent.length < maxDisplayStringLength) {
+        display.textContent += value;
+      }
     });
   });
 
   clearButton.addEventListener("click", () => {
     display.textContent = "";
-    //Clear memory
   });
 
   backButton.addEventListener("click", () => {
